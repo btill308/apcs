f98f6e2e9b1ee0ed138f029c2c55e87293fe01f0
btill308 <793913@seq.org>
2016-09-29 19:45:06 -0700

    Add files via upload

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -4,37 +4,37 @@
  * BallRunner
  */
 public class BallRunner{ 
-   TGPoint entrancePoint = new TGPoint();
-   BallBot[] ballBotArray = new BallBot[10];
-   int i;
-   public BallRunner(BallWorld ballWorld, TGPoint entrancePoint, BallBot[] ballBotArrayLength){
-       BallBot[] ballBotArray = new BallBot[10];
-   }
-   public static void activity1(){
-      BallWorld ballWorld = new BallWorld(1280, 800);
-      TGPoint startPoint = new TGPoint(0.0, 0.0);
-      double startHeading = 0.0;
-      int radius = 45;
-      BallBot ballBot = new BallBot(ballWorld, startPoint, startHeading, radius);
-      while(ballBot.getHeading() >= 0){
-         if(ballBot.canMoveForward(ballWorld) == true){
-            ballBot.moveForward();
-         }
-         else{
-            ballBot.setHeading(ballBot.getHeading() + (Math.random() * 360));
-         }
-         ballBot.setPixelsPerSecond((int)(Math.random() * 1200));
-         ballBot.setColor((int)(Math.random() * 32));
+    TGPoint entrancePoint = new TGPoint();
+    BallBot[] ballBotArray;
+    public BallRunner(BallWorld ballWorld, TGPoint entrancePoint, BallBot[] ballBotArrayLength){
+        BallBot[] ballBotArray = new BallBot[10];
+    }
+    public static void activity1(){
+        BallWorld ballWorld = new BallWorld(1280, 800);
+        TGPoint startPoint = new TGPoint(0.0, 0.0);
+        double startHeading = 0.0;
+        int radius = 45;
+        BallBot ballBot = new BallBot(ballWorld, startPoint, startHeading, radius);
+        while(ballBot.getHeading() >= 0){
+            if(ballBot.canMoveForward(ballWorld) == true){
+                ballBot.moveForward();
+            }
+            else{
+                ballBot.setHeading(ballBot.getHeading() + (Math.random() * 360));
+            }
+            ballBot.setPixelsPerSecond((int)(Math.random() * 1200));
+            ballBot.setColor((int)(Math.random() * 32));
         }
-   }
-//    public int freeBallBotIndex(){
-//        for(i=0;i<ballBotArray.length;i++){
-//            if(ballBotArray[i] = null){
-//                return i;
-//            }
-//            else{
-//                 return ballBotArray.length;
-//            }
-//         }
-//     }
+    }
+    public int freeBallBotIndex(){
+        for(int i = 0; i < ballBotArray.length; i++){
+            if(ballBotArray[i] == null){
+                return i;
+            }
+            else{
+                return ballBotArray.length;
+            }
+        }
+        return -1;
+    }
 }
